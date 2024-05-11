46dcc8bfb17841b607f3087d0f279378ee4aab45
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-05-11 18:32:35 +0900

    initial commit

---
+++ src/test/java/com/example/rewalk/RewalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rewalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
