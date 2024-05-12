a9f5df7a8caa8ad8832e0999594ce0210f235db5
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-05-12 21:33:28 +0900

    FIX:: annotate default application testcode

--- src/test/java/com/example/rework/ReworkApplicationTests.java
+++ src/test/java/com/example/rework/ReworkApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.rework;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ReworkApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.rework;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ReworkApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
