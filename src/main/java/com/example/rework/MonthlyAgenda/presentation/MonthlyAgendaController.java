de39a2133bd990d87ee35686f46cdce561cc1664
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2024-05-15 22:47:09 +0900

    FEAT:: Add api, controller

--- src/main/java/com/example/rework/MonthlyAgenda/presentation/MonthlyAgendaController.java
+++ src/main/java/com/example/rework/MonthlyAgenda/presentation/MonthlyAgendaController.java
@@ -1,14 +1,49 @@
 package com.example.rework.MonthlyAgenda.presentation;
 
+import com.example.rework.MonthlyAgenda.application.MonthlyAgendaService;
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaRequestDto;
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaResponseDto;
 import com.example.rework.MonthlyAgenda.restapi.MonthlyAgendaApi;
+import com.example.rework.config.security.SecurityUtils;
+import com.example.rework.global.common.CommonResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/v1/monthlyAgenda")
 @RequiredArgsConstructor
 @Slf4j
 public class MonthlyAgendaController implements MonthlyAgendaApi {
+    private final MonthlyAgendaService monthlyAgendaService;
+
+    @Override
+    public ResponseEntity<CommonResDto<?>> readMonthlyAgenda(String date, SecurityUtils securityUtils) {
+        MonthlyAgendaResponseDto.ReadMonthlyAgendaResponseDto result = monthlyAgendaService.readMonthlyAgenda(date, securityUtils.getCurrentUserId());
+        return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "이번달 아젠다 조회에 성고했습니다.", result));
+    }
+
+    @Override
+    public ResponseEntity<CommonResDto<?>> createMonthlyAgenda(MonthlyAgendaRequestDto.CreateMonthlyAgendaRequestDto createMonthlyAgendaRequestDto, SecurityUtils securityUtils) {
+        MonthlyAgendaResponseDto.CreateMonthlyAgendaResponseDto result = monthlyAgendaService.createMonthlyAgenda(createMonthlyAgendaRequestDto, securityUtils.getCurrentUserId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResDto<>(1, "이번달 아젠다 생성에 성공했습니다.", result));
+    }
+
+    @Override
+    public ResponseEntity<CommonResDto<?>> updateMonthlyAgenda(MonthlyAgendaRequestDto.UpdateMonthlyAgendaRequestDto updateMonthlyAgendaRequestDto, SecurityUtils securityUtils) {
+        MonthlyAgendaResponseDto.UpdateMonthlyAgendaResponseDto result = monthlyAgendaService.updateMonthlyAgenda(updateMonthlyAgendaRequestDto, securityUtils.getCurrentUserId());
+        return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "이번달 아젠달 수정에 성공했습니다.", result));
+    }
+
+    @Override
+    public ResponseEntity<CommonResDto<?>> deleteMonthlyAgenda(Long monthlyAgendaId, SecurityUtils securityUtils) {
+        boolean result = monthlyAgendaService.deleteMonthlyAgenda(monthlyAgendaId, securityUtils.getCurrentUserId());
+        return ResponseEntity.status(HttpStatus.OK).body(new CommonResDto<>(1, "이번달 아젠다 삭제에 성공했습니다.", result));
+    }
+
 }
--- src/main/java/com/example/rework/MonthlyAgenda/restapi/MonthlyAgendaApi.java
+++ src/main/java/com/example/rework/MonthlyAgenda/restapi/MonthlyAgendaApi.java
@@ -1,4 +1,65 @@
 package com.example.rework.MonthlyAgenda.restapi;
 
+import com.example.rework.config.security.SecurityUtils;
+import com.example.rework.global.common.CommonResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+import static com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaRequestDto.*;
+
+@Tag(name = "이번달 아젠다 API", description = "이번달 아젠다 관련 API")
+@RestController
+@RequestMapping("/api/v1/monthlyAgenda")
+@Validated
 public interface MonthlyAgendaApi {
+
+    @Operation(
+            summary = "이번달 아젠다 조회",
+            description = "사용자 정보를 받아 해당하는 달의 '이번달 아젠다를' 조회하는 API"
+    )
+    @GetMapping("/read/{date}")
+    ResponseEntity<CommonResDto<?>> readMonthlyAgenda(
+            @PathVariable String date,
+            SecurityUtils securityUtils
+    );
+
+    @Operation(
+            summary = "이번달 아젠다 등록",
+            description = "사용자가 입력한 이번달 아젠다를 저장하는 API"
+    )
+    @PostMapping("/create")
+    ResponseEntity<CommonResDto<?>> createMonthlyAgenda(
+            @Valid
+            @RequestBody
+            CreateMonthlyAgendaRequestDto createMonthlyAgendaRequestDto,
+            SecurityUtils securityUtils
+    );
+
+    @Operation(
+            summary = "이번달 아젠다 수정",
+            description = "사용자가 설정한 이번달 아젠다를 수정하는 API"
+    )
+    @PutMapping("/update")
+    ResponseEntity<CommonResDto<?>> updateMonthlyAgenda(
+            @RequestBody
+            UpdateMonthlyAgendaRequestDto updateMonthlyAgendaRequestDto,
+            SecurityUtils securityUtils
+    );
+
+
+    @Operation(
+            summary = "이번달 아젠다 삭제",
+            description = "이번달 아젠다를 삭제하는 API, 현재는 필요하지 않음."
+    )
+    @DeleteMapping("/delete")
+    ResponseEntity<CommonResDto<?>> deleteMonthlyAgenda(
+            @RequestParam("monthlyAgendaId") Long monthlyAgendaId,
+            SecurityUtils securityUtils
+    );
 }
