7238e0ad1adcbcb707ad9600ca290d874f525dae
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2024-05-15 22:47:47 +0900

    FEAT:: add service

--- src/main/java/com/example/rework/MonthlyAgenda/application/MonthlyAgendaService.java
+++ src/main/java/com/example/rework/MonthlyAgenda/application/MonthlyAgendaService.java
@@ -1,4 +1,14 @@
 package com.example.rework.MonthlyAgenda.application;
 
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaRequestDto;
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaResponseDto;
+
 public interface MonthlyAgendaService {
+    MonthlyAgendaResponseDto.ReadMonthlyAgendaResponseDto readMonthlyAgenda(String date, Long currentUserId);
+
+    MonthlyAgendaResponseDto.CreateMonthlyAgendaResponseDto createMonthlyAgenda(MonthlyAgendaRequestDto.CreateMonthlyAgendaRequestDto createMonthlyAgendaRequestDto, Long currentUserId);
+
+    MonthlyAgendaResponseDto.UpdateMonthlyAgendaResponseDto updateMonthlyAgenda(MonthlyAgendaRequestDto.UpdateMonthlyAgendaRequestDto updateMonthlyAgendaRequestDto, Long currentUserId);
+
+    boolean deleteMonthlyAgenda(Long monthlyAgendaId, Long currentUserId);
 }
--- src/main/java/com/example/rework/MonthlyAgenda/application/impl/MonthlyAgendaServiceImpl.java
+++ src/main/java/com/example/rework/MonthlyAgenda/application/impl/MonthlyAgendaServiceImpl.java
@@ -1,6 +1,39 @@
 package com.example.rework.MonthlyAgenda.application.impl;
 
 import com.example.rework.MonthlyAgenda.application.MonthlyAgendaService;
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaRequestDto;
+import com.example.rework.MonthlyAgenda.application.dto.MonthlyAgendaResponseDto;
+import com.example.rework.MonthlyAgenda.domain.MonthlyAgenda;
+import com.example.rework.MonthlyAgenda.domain.repository.MonthlyAgendaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
 public class MonthlyAgendaServiceImpl implements MonthlyAgendaService {
+    private final MonthlyAgendaRepository monthlyAgendaRepository;
+    @Override
+    public MonthlyAgendaResponseDto.ReadMonthlyAgendaResponseDto readMonthlyAgenda(String date, Long currentUserId) {
+        return null;
+    }
+
+    @Override
+    public MonthlyAgendaResponseDto.CreateMonthlyAgendaResponseDto createMonthlyAgenda(MonthlyAgendaRequestDto.CreateMonthlyAgendaRequestDto createMonthlyAgendaRequestDto, Long currentUserId) {
+        return null;
+
+    }
+
+    @Override
+    public MonthlyAgendaResponseDto.UpdateMonthlyAgendaResponseDto updateMonthlyAgenda(MonthlyAgendaRequestDto.UpdateMonthlyAgendaRequestDto updateMonthlyAgendaRequestDto, Long currentUserId) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteMonthlyAgenda(Long monthlyAgendaId, Long currentUserId) {
+        return false;
+    }
 }
