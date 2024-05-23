e6100140aed9458045ba82bcd39cb4378be50f2b
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-05-23 17:20:42 +0900

    feat: restful 규칙 적용

--- src/main/java/com/example/rework/MonthlyAgenda/restapi/MonthlyAgendaApi.java
+++ src/main/java/com/example/rework/MonthlyAgenda/restapi/MonthlyAgendaApi.java
@@ -23,7 +23,7 @@ public interface MonthlyAgendaApi {
             summary = "이번달 아젠다 조회",
             description = "사용자 정보와 연,월을 받아 해당하는 달의 '이번달 아젠다를' 조회하는 API"
     )
-    @GetMapping("/read")
+    @GetMapping("/")
     ResponseEntity<CommonResDto<?>> readMonthlyAgenda(
             @RequestBody ReadMonthlyAgendaRequestDto readMonthlyAgendaRequestDto,
             SecurityUtils securityUtils
@@ -33,7 +33,7 @@ public interface MonthlyAgendaApi {
             summary = "이번달 아젠다 등록",
             description = "사용자가 입력한 이번달 아젠다를 저장하는 API"
     )
-    @PostMapping("/create")
+    @PostMapping("/")
     ResponseEntity<CommonResDto<?>> createMonthlyAgenda(
             @Valid
             @RequestBody
@@ -45,7 +45,7 @@ public interface MonthlyAgendaApi {
             summary = "이번달 아젠다 수정",
             description = "사용자가 설정한 이번달 아젠다를 수정하는 API"
     )
-    @PutMapping("/update")
+    @PutMapping("/")
     ResponseEntity<CommonResDto<?>> updateMonthlyAgenda(
             @RequestBody
             UpdateMonthlyAgendaRequestDto updateMonthlyAgendaRequestDto,
@@ -57,7 +57,7 @@ public interface MonthlyAgendaApi {
             summary = "이번달 아젠다 삭제",
             description = "이번달 아젠다를 삭제하는 API, 현재는 필요하지 않음."
     )
-    @DeleteMapping("/delete")
+    @DeleteMapping("/")
     ResponseEntity<CommonResDto<?>> deleteMonthlyAgenda(
             @RequestParam("monthlyAgendaId") Long monthlyAgendaId,
             SecurityUtils securityUtils
