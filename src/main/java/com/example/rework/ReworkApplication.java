b7b31ecfdb9e933c22dcec1f7ee561db591f2aad
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2024-05-28 23:35:02 +0900

    FEAT:: 미완료 아젠다 복사 기능 구현

--- src/main/java/com/example/rework/ReworkApplication.java
+++ src/main/java/com/example/rework/ReworkApplication.java
@@ -2,8 +2,10 @@ package com.example.rework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReworkApplication.class, args);
